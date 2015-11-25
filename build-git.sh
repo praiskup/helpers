56ca71f9234fce9d1f6bb1421a49ca8e569b3478
Pavel Raiskup <praiskup@redhat.com>
2015-11-25 19:51:39 +0100

    fix build-git.sh for the first autoreconf

--- build-git.sh
+++ build-git.sh
@@ -5,6 +5,11 @@ set -e
 gitdir=$(dirname "$0")
 cd "$gitdir"
 
+for i in bin libexec share share/eimg
+do
+    target="$i" build-aux/gen-makefile-am > "$i/Makefile.inc"
+done
+
 autoreconf -vfi
 ./configure "--prefix=$(pwd)"
 make
